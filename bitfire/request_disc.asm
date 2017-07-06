14824f9f68bc7deaee95d5676eb458d1cc5b8535
dotscha <dotsch@gmail.com>
2017-07-06 13:57:52 +0200

    Checking drive type runtime instead of compile time.

--- bitfire/request_disc.asm
+++ bitfire/request_disc.asm
@@ -1,14 +1,10 @@
-!ifndef BITFIRE_IS_INCLUDED {
-!src "loader_acme.inc"
-}
+
 bitfire_request_disc_
 		;send $fx as command
 		;ldx #$07
 		jsr bitfire_send_byte_
 
 !if (BITFIRE_PLATFORM = BITFIRE_C64) {
-		;bit $dd00
-		;bmi *-3
 
 		;give floppy time to enter busy mode, and set lines to input
 		lda #$3f
@@ -17,12 +13,13 @@ bitfire_request_disc_
 		;wait until floppy is idle again
                 bit $dd00
                 bpl *-3
-.waste
-		rts
+
 } else {
-  !if (BF_DRIVE = 1541) {				;===== 1541
-		;bit $01
-		;bmi *-2
+
+                bit link_drive_type
+		bmi .case1551
+
+                ;===== 1541
 
 		;give floppy time to enter busy mode, and set lines to input
 		lda #%11001000
@@ -31,7 +28,11 @@ bitfire_request_disc_
 		;wait until floppy is idle again
                 bit $01
                 bpl *-2
-  } else {						;===== 1551
+
+		bmi .waste
+
+.case1551       ;===== 1551
+
 		txa
 		ldx	#0
 		dex
@@ -39,7 +40,8 @@ bitfire_request_disc_
 		tax
 		bit	$fef0
 		bpl	*-3
-  }
+
+}
+
 .waste
 		rts
-}
--- bitfire/reset_drive.asm
+++ bitfire/reset_drive.asm
@@ -1,30 +1,35 @@
-!ifndef BITFIRE_IS_INCLUDED {
-!src "loader_acme.inc"
-}
+
 bitfire_reset_drive_
 !zone {
                 ldy #.upload_size - 1
 		;check if drive is idle
+
 !if (BITFIRE_PLATFORM = BITFIRE_C64) {
                 bit $dd00
                 bpl *-3
 } else {
-  !if (BF_DRIVE = 1541) {				;===== 1541
+
+                bit link_drive_type
+                bmi +
+
+                ;===== 1541
 		bit	$01
 		bpl	*-2
-  } else {						;===== 1551
+                bmi ++
+
+                ;===== 1551
++
 		bit	$fef0
 		bpl	*-3
-  }
++
 }
 
 		;upload data
 -
-        lda .upload_start,y
-        jsr bitfire_send_byte_
-		;waste cycles to be sure drive keeps up
+                lda .upload_start,y
+                jsr bitfire_send_byte_
 		dey
-        bpl -
+                bpl -
 .waste
 		rts
 
@@ -32,15 +37,14 @@ bitfire_reset_drive_
 !pseudopc $0108 {
 .code_start
 		;code to be uploaded goes here
-		;jmp $eaa0
 		jmp ($fffc)
 .code_end
 }
 .code_size = .code_end - .code_start;
 
-        !byte >(.code_size - 1), <(.code_size - 1)
-		!byte >(.code_end - 1),  <(.code_end - 1)
-		!byte BITFIRE_UPLOAD
+                !byte >(.code_size - 1), <(.code_size - 1)
+                !byte >(.code_end - 1),  <(.code_end - 1)
+                !byte BITFIRE_UPLOAD
 .upload_end
 .upload_size = .upload_end - .upload_start;
 }
